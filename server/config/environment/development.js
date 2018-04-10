9821a585ce63f030d23edd0e6fe365ab0a5cbf0b
Rani Dhole <RD00486730@TechMahindra.com>
2018-04-10 20:19:13 +0530

    rani

---
+++ server/config/environment/development.js
@@ -0,0 +1,26 @@
+'use strict';
+/*eslint no-process-env:0*/
+
+// Development specific configuration
+// ==================================
+module.exports = {
+    // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/rani-dev'
+  },
+
+    // Sequelize connection opions
+  sequelize: {
+    uri: 'sqlite://',
+    options: {
+      logging: false,
+      storage: 'dev.sqlite',
+      define: {
+        timestamps: false
+      }
+    }
+  },
+
+    // Seed database on startup
+  seedDB: true
+};
